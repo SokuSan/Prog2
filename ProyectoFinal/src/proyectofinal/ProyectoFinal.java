06f92dce6054241e5b51f19b479e18aed0950183
Elnoobtriunfador <pedrohm2704@gmail.com>
2025-04-23 14:15:25 +0200

    cambios de proyecto 2

--- ProyectoFinal/src/proyectofinal/ProyectoFinal.java
+++ ProyectoFinal/src/proyectofinal/ProyectoFinal.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tallerfinal2;
+package proyectofinal;
 
 /**
  *
  * @author Pedro
  */
-public class TallerFinal2 {
+public class ProyectoFinal {
 
     /**
      * @param args the command line arguments
